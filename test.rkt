6d082062fb523a87965824d060e70206e4393c95
1e1001 <michael@malinov.com>
2023-03-17 22:58:02 -0700

    transfer

--- test.rkt
+++ test.rkt
@@ -1,6 +1,5 @@
 #lang tpl
 @require[racket/format]
-@#%tpl-lift[(displayln "this expression is lifted")]
 @tpl-output[displayln]
 this is a test tpl file
-@~v[@tpl-run["."]]
+@tpl-run["."]
--- tpl/main.rkt
+++ tpl/main.rkt
@@ -17,13 +17,22 @@
           (all-from-out racket/base)
           #%module-begin)
          (rename-out
-          [-module-begin #%module-begin])
+          [-module-begin #%module-begin]
+          [tpl-doc ~*])
          tpl-file
          tpl-output
          tpl-run
          tpl-run*
          #%tpl-lift
-         output/file)
+         output/file
+         ~when
+         ~unless
+         ~)
+
+(struct tpl-doc (v) #:transparent)
+
+{define (~ . args)
+  (tpl-doc args)}
 
 {define-syntax-rule (#%tpl-lift . body)
   {begin . body}}
@@ -39,50 +48,47 @@
 {define-syntax (-module-begin stx)
   {parameterize ([current-namespace (make-base-namespace)])
     (define exprs (syntax-e (cdr (syntax-e stx))))
-    (define push-len 0)
-    (define res
-      {for/list ([expr exprs])
-        (if {let/cc brk
-              (define expanded (syntax-e (expand expr)))
-              (writeln expanded)
-              {unless (pair? expanded)
-                (brk #f)}
-              (define name (syntax-e (car expanded)))
-              (or (eq? name '#%expression)
-                  (eq? name '#%provide)
-                  (eq? name '#%declare)
-                  (eq? name 'module)
-                  (eq? name 'module*)
-                  (eq? name 'module+)
-                  (eq? name 'define-values)
-                  (eq? name 'define-syntaxes)
-                  (eq? name '#%require)
-                  (and (eq? name '#%app)
-                       (syntax? (cdr expanded))
-                       {let ([in (syntax-e (cdr expanded))])
-                         (and (pair? in)
-                              (pair? (car in))
-                              (eq? (caar in) '#%top)
-                              (eq? (cdar in) '#%tpl-lift))}))}
-            expr
-            {begin0
-              #`(vector-set! res #,push-len #,expr)
-              (set! push-len (add1 push-len))})})
-    (writeln res)
+    (define body-exprs null)
+    (define item-exprs null)
+    {for/list ([expr exprs])
+      (if {let/cc brk
+            (define expanded (syntax-e (expand expr)))
+            {unless (pair? expanded)
+              (brk #f)}
+            (define name (syntax-e (car expanded)))
+            (or (eq? name '#%expression)
+                (eq? name '#%provide)
+                (eq? name '#%declare)
+                (eq? name 'module)
+                (eq? name 'module*)
+                (eq? name 'module+)
+                (eq? name 'define-values)
+                (eq? name 'define-syntaxes)
+                (eq? name '#%require)
+                (and (eq? name '#%app)
+                     (syntax? (cdr expanded))
+                     {let ([in (syntax-e (cdr expanded))])
+                       (and (pair? in)
+                            (pair? (car in))
+                            (eq? (caar in) '#%top)
+                            (eq? (cdar in) '#%tpl-lift))}))}
+          (set! body-exprs (cons expr body-exprs))
+          (set! item-exprs (cons expr item-exprs)))}
     #`{#%module-begin
        (define #%tpl-script-out #f)
        (provide #%tpl-script-out)
-       (define res (vector #,push-len))
-       #,@res
-       (set! #%tpl-script-out (apply tpl-file res))}}}
+       #,@(reverse body-exprs)
+       (set! #%tpl-script-out (tpl-file . #,(reverse item-exprs)))}}}
 
 ; convert an @-exp list into a string
 {define (stringify item)
   (cond
     [(string? item) item]
     [(void? item) ""]
-    [(list? item)
-     (apply string-append (map stringify item))]
+    [(tpl-doc? item)
+     (apply string-append
+            {for/list ([i (tpl-doc-v item)])
+              (stringify i)})]
     [else (~s item)])}
 
 (define output-func (make-parameter #f))
@@ -95,39 +101,50 @@
 
 {define-syntax-rule (tpl-file . body)
   {parameterize ([output-func #f])
-    (define res (stringify (list . body)))
+    (define res (stringify (~ . body)))
     (define out (output-func))
     {unless out
       (error "tpl-file call did not specify an output")}
     (apply (car out) res (cdr out))}}
 
 {define (output/file res path)
-  (error "todo")}
+  (call-with-output-file
+   path #:exists 'truncate/replace
+   {λ (port)
+     (display res port)})}
 
 {define (tpl-run path)
   {define (collect-paths path)
     (case (file-or-directory-type path)
-      [(file link) (if (path-has-extension? path #".rkt") path '())]
+      [(file link)
+       (if (and (path-has-extension? path #".rkt")
+                (call-with-input-file
+                 path
+                 {λ (port)
+                   (define lang (read-language port {λ () #f}))
+                   (and lang (eq? (lang 'module-language #f) 'tpl))}))
+           path
+           '())]
       [(directory directory-link)
-       (map collect-paths
-            (directory-list path #:build? #t))])}
+       (filter {λ (v)
+                 (not (null? v))}
+               (map collect-paths
+                    (directory-list path #:build? #t)))])}
   {define (inner-run path)
     (cond
+      [(null? path) #f]
       [(list? path) (map inner-run path)]
-      [path
-       (if (call-with-input-file
-           path
-           {λ (port)
-             (define lang (read-language port {λ () #f}))
-             (and lang (eq? (lang 'module-language #f) 'tpl))})
-           (dynamic-require (make-resolved-module-path (normalize-path path)) '#%tpl-script-out)
-           #f)]
-      [else #f])}
-  {define (clean-res res)
-    (if (list? res)
-        (map clean-res (filter values res))
-        res)}
-  (clean-res (inner-run (collect-paths path)))}
+      [else (cons path (dynamic-require (make-resolved-module-path
+                                         (normalize-path path))
+                                        '#%tpl-script-out
+                                        {λ () #f}))])}
+  (inner-run (collect-paths path))}
 
 {define (tpl-run* . paths)
   (map tpl-run paths)}
+
+{define-syntax-rule (~when cond . args)
+  (if cond (list . args) null)}
+{define-syntax-rule (~unless cond . args)
+ (if cond null (list . args))}
+
