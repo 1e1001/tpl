2ad2bbe49bc8f1c67b310b5e783aacb52b11f502
1e1001 <michael@malinov.com>
2023-05-12 13:43:30 -0700

    more features and actually usable now

---
+++ tpl/dotutils.rkt
@@ -0,0 +1,78 @@
+#lang racket/base
+; tpl/dotutils - utilities for use as a configuration manager
+(require tpl
+         file/glob
+         racket/format
+         racket/string)
+{define+provide (output/if-newer path inner)
+  (if (> (file-or-directory-modify-seconds (current-tpl-script))
+         (file-or-directory-modify-seconds path #f {λ () 0}))
+      (inner path)
+      ; returning `void` instead of `(void)` is intentional, `void` is a "valid" output
+      void)}
+
+{define (inner-launch #:stdio [stdio '(#f #f #f)] #:during [during void] #:after [after void] . exec)
+  (define-values (subproc stdout stdin stderr)
+    (apply
+     subprocess
+     (if (car stdio) #f (current-output-port))
+     (if (cadr stdio) #f (current-input-port))
+     (if (caddr stdio) #f (current-error-port))
+     exec))
+  (during subproc stdout stdin stderr)
+  {when (car stdio) (close-input-port stdout)}
+  {when (cadr stdio) (close-output-port stdin)}
+  {when (caddr stdio) (close-input-port stderr)}
+  (subprocess-wait subproc)
+  (after subproc)
+  {unless (eq? (subprocess-status subproc) 0)
+    (error "system call failed")}
+}
+
+{define+provide ((output/file-as-root path) body)
+  (case (system-type 'os)
+    [(unix macos)
+     (printf "→ root: write file ~a\n" path)
+     (inner-launch
+      (find-executable-path "sudo")
+      (find-executable-path "dd")
+      (bytes-append #"of=" (path->bytes (if (path? path) path (string->path path))))
+      #:stdio '(#f #t #f)
+      #:during {λ (_0 _1 stdin _2)
+             (display (tpl-doc->string (body)) stdin)}
+      #:after {λ (_0) (printf "← root…\n")})]
+    [else (error "output/file-as-root is only supported on unix, got" (system-type 'os))])}
+
+{define (resolve-command-like cmd)
+  (cond
+    [(path? cmd) cmd]
+    [(string? cmd) (find-executable-path cmd)]
+    [(list? cmd) (car cmd)])}
+
+{define (resolve-exec-help help args)
+  (case help
+    [(#f) (printf "~a…\n" (car args))]
+    [(#t) (printf "~a\n" (string-join (map ~a args) " "))]
+    [else (printf "~a\n" help)])}
+
+{define+provide (exec-as-root help cmd . args)
+  (printf "→ root: ")
+  (resolve-exec-help help (cons cmd args))
+  (apply
+   inner-launch
+   (find-executable-path "sudo")
+   (resolve-command-like cmd)
+   args
+   #:after {λ _ (printf "← root…\n")})}
+
+{define+provide (exec help cmd . args)
+  (printf "→ exec: ")
+  (resolve-exec-help help (cons cmd args))
+  (apply
+   inner-launch
+   (resolve-command-like cmd)
+   args
+   #:after {λ _ (printf "← exec…\n")})}
+
+{define+provide (tpl-run-glob pattern)
+  (map tpl-run (glob pattern #:capture-dotfiles? #t))}
--- tpl/main.rkt
+++ tpl/main.rkt
@@ -1,4 +1,7 @@
 #lang racket/base
+; tpl - main entry point & lang
+(require racket/format
+         racket/path)
 
 ; this is functionally equivalent to doing
 ;   #lang at-exp <lang>
@@ -7,31 +10,34 @@
 ; but cooler (allows lang-detection in tpl-run)
 (module reader syntax/module-reader
   #:language read
-  #:wrapper2 {λ (in rd stx?)
-               {parameterize ([current-readtable
-                               (make-at-readtable)])
-                 (define mod (rd in))
-                 (define mod-e (syntax-e mod))
-                 (define beg (cadddr mod-e))
-                 (define beg-e (syntax-e beg))
-                 (datum->syntax
-                  mod
-                  (list (car mod-e)
-                        (cadr mod-e)
-                        (caddr mod-e)
-                        (datum->syntax
-                         beg
-                         (list* (car beg-e)
-                                ; #'(require tpl) would be more correct here,
-                                ; but that results in an ambiguous identifier or something
-                                (datum->syntax beg '(require tpl))
-                                (cdr beg-e))
-                         beg))
-                  mod)}}
+  #:info
+  {λ (key default parent)
+    (if (eq? key 'tpl-script?)
+        #t
+        (parent key default))}
+  #:wrapper2
+  {λ (in rd stx?)
+    {parameterize ([current-readtable
+                    (make-at-readtable)])
+      (define mod (rd in))
+      (define mod-e (syntax-e mod))
+      (define beg (cadddr mod-e))
+      (define beg-e (syntax-e beg))
+      (datum->syntax
+       mod
+       (list (car mod-e)
+             (cadr mod-e)
+             (caddr mod-e)
+             (datum->syntax
+              beg
+              (list* (car beg-e)
+                     ; #'(require tpl) would be more correct here,
+                     ; but that results in an ambiguous identifier or something
+                     (datum->syntax beg '(require tpl))
+                     (cdr beg-e))
+              beg))
+       mod)}}
   (require scribble/reader))
-
-(require racket/format
-         racket/path)
 
 ; define & provide in the same expression
 (provide define+provide)
@@ -56,14 +62,12 @@
   (tpl-doc args)}
 
 ; convert an @-exp list into a string
-{define+provide (stringify item)
+{define+provide (tpl-doc->string item)
   (cond
     [(string? item) item]
     [(void? item) ""]
     [(tpl-doc? item)
-     (apply string-append
-            {for/list ([i (tpl-doc-v item)])
-              (stringify i)})]
+     (apply string-append (map tpl-doc->string (tpl-doc-v item)))]
     [else (~s item)])}
 
 ; "@:" expressions are like the normal ones but specifically for tpl-doc's
@@ -82,18 +86,21 @@
 ; different types of output
 ; in the format of ((output/whatever . args) body) or (output/whatever body)
 {define+provide-syntax-rule (output/module body)
-  (define+provide #%tpl-output/module body)}
+  (define+provide #%tpl-output/module (body))}
 {define+provide ((output/file path) body)
   (call-with-output-file
    path #:exists 'truncate/replace
    {λ (port)
-     (display (stringify body) port)})}
+     (display (tpl-doc->string (body)) port)})}
 {define+provide ((output/call fn . args) body)
-  (apply fn (stringify body) args)}
+  (apply fn (tpl-doc->string (body)) args)}
 
 ; defines a document
 {define+provide-syntax-rule (tpl out-fn . body)
-  (out-fn (: . body))}
+  (out-fn {λ () (: . body)})}
+
+; current executing tpl script
+(define+provide current-tpl-script (make-parameter (find-system-path 'run-file) #f 'current-tpl-script))
 
 ; recurse tpl scripts
 {define+provide (tpl-run path)
@@ -101,26 +108,34 @@
     {let collect-paths ([path path])
       (case (file-or-directory-type path)
         [(file link)
-         (if (and (path-has-extension? path #".rkt")
+         (if (and (or (path-has-extension? path #".rkt")
+                      (path-has-extension? path #".tpl"))
                   (call-with-input-file
                    path
                    {λ (port)
                      (define lang (read-language port {λ () #f}))
-                     (and lang (eq? (lang 'module-language #f) 'tpl))}))
+                     (and lang (lang 'tpl-script? #f))}))
              path
              '())]
         [(directory directory-link)
          (filter {λ (v)
                    (not (null? v))}
                  (map collect-paths
-                               (directory-list path #:build? #t)))])})
+                     (directory-list path #:build? #t)))])})
   {let inner-run ([path paths])
     (cond
       [(null? path) #f]
       [(list? path) (map inner-run path)]
-      [else (cons path (dynamic-require
-                        (make-resolved-module-path (normalize-path path))
-                        '#%tpl-output/module
-                        {λ () #f}))])}}
+      [else
+       (define mod-path (make-resolved-module-path (normalize-path path)))
+       {parameterize ([current-tpl-script path])
+         (dynamic-require
+          mod-path
+          0
+          {λ () #f})
+         (cons path (dynamic-require
+                     mod-path
+                     '#%tpl-output/module
+                     {λ () #f}))}])}}
 {define+provide (tpl-run* . paths)
   (map tpl-run paths)}
